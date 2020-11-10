7663e9694e752672ad40a13a79428f6374fef584
Choops <choopsbd@gmail.com>
2020-11-10 15:54:43 +0100

    WIP

---
+++ nextcloud.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+import os
+import _myhelpers_ as myh
+
+__description__ = "Install Debian Nextcloud server"
+__author__ = "Choops <choopsbd@gmail.com>"
+
+
+def usage():
+    myscript = f"{os.path.basename(sys.argv[0])}"
+    print(f"{ci}{__description__}\nUsage{c0}:")
+    print(f"  './{myscript} [OPTION]' as root or using 'sudo'")
+    print(f"{ci}Options{c0}:")
+    print(f"  -h,--help: Print this help")
+    print()
+
+
+def prerequisites():
+    distro = ""
+    codename = ""
+
+    with open("/etc/os-release", "r") as f:
+        for line in f:
+            if line.startswith("ID="):
+                distro = line.split("=")[1].rstrip()
+            if line.startswith("VERSION_CODENAME="):
+                codename = line.split("=")[1].rstrip()
+
+    if distro != "debian":
+        print(f"{error} OS is not Debian")
+        exit(1)
+
+    if os.getuid() != 0:
+        print(f"{error} Need higher privileges")
+        exit(1)
+
+    if codename in olddebian:
+        print(f"{error} '{codename}' is a too old Debian version")
+        exit(1)
+
+    if codename != debianstable:
+        print(f"{warning} You are using a testing/unstable version of Debian.")
+        okgo = input("Continue despite it could be unstable [y/N] ? ")
+        if not re.match('^(y|yes)', okgo.lower()):
+            exit(0)
+
+
+def configure_server():
+    print(f"{ci}Configuring server...{c0}")
+    myh.common_config()
+
+
+c0 = "\33[0m"
+ce = "\33[31m"
+cok = "\33[32m"
+cw = "\33[33m"
+ci = "\33[36m"
+
+error = f"{ce}E{c0}:"
+done = f"{cok}OK{c0}:"
+warning = f"{cw}W{c0}:"
+
+srcfolder = os.path.dirname(os.path.realpath(__file__))
+
+olddebian = ["stretch", "jessie", "wheezy", "squeeze", "lenny"]
+debianstable = "buster"
+
+currentpath = os.getcwd()
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if re.match('^-(h|-help)$', sys.argv[1]):
+            usage()
+            exit(0)
+        else:
+            print(f"{error} Bad argument")
+            usage()
+            exit(1)
+
+    prerequisites()
+
+    myhostname, mydomain = myh.set_hostname()
+    myiface, myoldip, myip, renewip = myh.set_ipaddr()
+
+    print(f"\n{ci}Server settings{c0}:")
+    print(f"  - {ci}Hostname{c0}:   {myhostname}")
+    print(f"  - {ci}Domain{c0}:     {mydomain}")
+    if renewip:
+        print(f"  - {ci}IP address{c0}: {myip}")
+    print(f"{ci}service settings{c0}:")
+
+    confconf = input("Confirm configuration [Y/n] ? ")
+    if re.match('^(n|no)$', confconf):
+        exit(0)
+
+    myh.renew_hostname(myhostname, mydomain)
+    if renewip:
+        myh.fix_ip(myiface, myip, myoldip)
+
+    mypkgs = ["apache2", "libapache2-mod-php", "mariadb-server", "php-xml",
+              "php-cli", "php-cgi", "php-mysql", "php-mbstring",
+              "php-gd php-curl", "php-zip"]
+    myh.install_server(mypkgs)
+
+    configure_server()
